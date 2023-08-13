ede3bdd46be991937e2a4c33a63357e817ee2268
limbold <2695333484@qq.com>
2023-08-13 13:57:38 +0800

    a game demo

---
+++ Source/ICDemo/ICDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ICDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ICDEMO_API AICDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
